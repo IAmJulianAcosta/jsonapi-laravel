ef2346325fd91e4d4301487f11531ca1e1294d33
Julian Acosta <iam@julianacosta.me>
2017-01-02 16:26:30 -0500

    Adding token to login response

--- src/EchoIt/JsonApi/Auth/RegistersUsers.php
+++ src/EchoIt/JsonApi/Auth/RegistersUsers.php
@@ -8,6 +8,9 @@
 	
 	namespace EchoIt\JsonApi\Auth;
 	
+	use EchoIt\JsonApi\Http\Response;
+	use Illuminate\Auth\TokenGuard;
+	use Illuminate\Contracts\Auth\Guard;
 	use Illuminate\Http\Request;
 	use Illuminate\Contracts\Auth\Authenticatable;
 	use Illuminate\Auth\Events\Registered;
@@ -23,5 +26,15 @@
 			event(new Registered($user));
 			
 			$this->guard()->login($user, true);
+		}
+		
+		protected function userRegistered(Request $request, Authenticatable $user, Response $response) {
+			/** @var Guard $guard */
+			$guard = $this->guard();
+			if ($guard instanceof TokenGuard === true) {
+				$response->meta = [
+					"token" => $user->getRememberToken ()->token
+				];
+			}
 		}
 	}
