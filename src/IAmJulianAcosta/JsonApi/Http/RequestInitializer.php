dcfe05330320646f5189a33370804e7bc9c907bd
Julian Acosta <iam@julianacosta.me>
2017-02-19 18:07:07 -0500

    Fixed code style, extra type checking

--- src/IAmJulianAcosta/JsonApi/Http/RequestInitializer.php
+++ src/IAmJulianAcosta/JsonApi/Http/RequestInitializer.php
@@ -23,10 +23,16 @@
 		/**
 		 *
 		 */
-		static protected function getFieldsParametersFromRequest (Request &$request) {
+		protected static function getFieldsParametersFromRequest (Request &$request) {
 			$fieldsCollection = new Collection();
-			foreach ($request->input('fields') as $model => $fields) {
-				$fieldsCollection->put($model, array_filter(explode(',', $fields)));
+			$fields = $request->input('fields');
+			if (is_array($fields) === true) {
+				foreach ($fields as $model => $field) {
+					$fieldsCollection->put($model, array_filter(explode(',', $field)));
+				}
+			}
+			else {
+				Exception::throwSingleException('Fields parameter must be an array', 0, Response::HTTP_BAD_REQUEST);
 			}
 			$request->setFields($fieldsCollection);
 		}
@@ -34,42 +40,47 @@
 		/**
 		 *
 		 */
-		static protected function initializeInclude(Request &$request) {
+		protected static function initializeInclude(Request &$request) {
 			$request->setInclude(($parameter = $request->input('include')) ? explode(',', $parameter) : []);
 		}
 		
 		/**
 		 *
 		 */
-		static protected function initializeSort(Request &$request) {
+		protected static function initializeSort(Request &$request) {
 			$request->setSort(($parameter = $request->input('sort')) ? explode(',', $parameter) : []);
 		}
 		
 		/**
 		 *
 		 */
-		static protected function initializeFilter(Request &$request) {
+		protected static function initializeFilter(Request &$request) {
 			$request->setFilter(($parameter = $request->input('filter')) ? (is_array($parameter) ? $parameter : explode(',', $parameter)) : []);
 		}
 		
 		/**
 		 *
 		 */
-		static protected function initializePage(Request &$request) {
+		protected static function initializePage(Request &$request) {
 			$page = $request->input('page') ? $request->input('page') : [];
 			
-			static::checkIfPageIsValid($page);
-			
-			$request->setPage($page);
-			$request->setPageSize((integer)$page['size']);
-			$request->setPageNumber((integer)$page['number']);
+			if (is_array($page) === false) {
+				Exception::throwSingleException('Page parameter must be an array', 0, Response::HTTP_BAD_REQUEST);
+			}
+			else {
+				static::checkIfPageIsValid($page);
+				
+				$request->setPage($page);
+				$request->setPageSize((integer)$page['size']);
+				$request->setPageNumber((integer)$page['number']);
+			}
 		}
 		
 		/**
 		 * @param $page
 		 */
-		static protected function checkIfPageIsValid($page) {
-			if (is_array($page) === false || empty($page['size']) === true || empty($page['number']) === true) {
+		protected static function checkIfPageIsValid($page) {
+			 if (empty($page['size']) === true || empty($page['number']) === true) {
 				Exception::throwSingleException('Expected page[size] and page[number]', 0, Response::HTTP_BAD_REQUEST);
 			}
 		}
